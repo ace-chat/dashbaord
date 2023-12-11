f4b04ff29ab1013379f35c9fa66ec8382c48b6b1
EsraaSaad298 <47531570+EsraaSaad298@users.noreply.github.com>
2023-12-11 19:05:52 +0400

    Change Number Modal ..

--- src/components/Modal/ChangeNumber.tsx
+++ src/components/Modal/ChangeNumber.tsx
@@ -1,9 +1,17 @@
-import { Button, Modal } from 'antd'
+import { Button, Input, Modal, Select } from 'antd'
 import { pxToVw } from '@/utils'
+import { useState } from 'react';
 
-export const ChangeNumber = ({t, changeNumber, toggleChangeNumber, oldNumber, setOldNumber}: any) => {
+export const ChangeNumber = ({t, changeNumber, toggleChangeNumber, oldNumber, setOldNumber, newNumber, setNewNumber, confirmNumber, setConfirmNumber}: any) => {
+    const [sampleCodes] = useState([
+        { value: "+971", label: "+971" },
+        { value: "+1", label: "+1" },
+        { value: "+20", label: "+20" },
+    ]);
+
     return(
         <Modal
+            className='number_modal'
             centered
             open={changeNumber}
             onCancel={toggleChangeNumber}
@@ -13,6 +21,7 @@ export const ChangeNumber = ({t, changeNumber, toggleChangeNumber, oldNumber, se
                     <div className='flex items-center justify-center'>
                         <Button
                             type="default"
+                            style={{ borderRadius: pxToVw(20), marginTop: pxToVw(30), marginBottom: pxToVw(30) }}
                             onClick={() => {
                                 
                             }}
@@ -26,9 +35,20 @@ export const ChangeNumber = ({t, changeNumber, toggleChangeNumber, oldNumber, se
         >
             <div style={{ fontFamily: "PingFang SC Medium", fontSize: pxToVw(22) }}>{t("Change Number")}</div>
             <div style={{ fontFamily: "PingFang SC Medium", fontSize: pxToVw(12), color: "#767676"  }}>{t("Verify and change the existing number with easy steps. ")}</div>
-            <div className='flex flex-row'>
+            <div className='flex flex-row' style={{ marginTop: pxToVw(24) }}>
                 {/* add country code */}
-                
+                <Select style={{ width: pxToVw(70), height: pxToVw(35) }} options={sampleCodes} placeholder={"+971"} />
+                <Input styles={{ input: { width: pxToVw(422), height: pxToVw(35), fontSize: pxToVw(10), marginLeft: pxToVw(18) } }} placeholder={t('Old number')} value={oldNumber} onChange={(e) => setOldNumber(e.target.value)} />
+            </div>
+            <div className='flex flex-row' style={{ marginTop: pxToVw(20) }}>
+                {/* add country code */}
+                <Select style={{ width: pxToVw(70), height: pxToVw(35) }} options={sampleCodes} placeholder={"+971"} />
+                <Input styles={{ input: { width: pxToVw(422), height: pxToVw(35), fontSize: pxToVw(10), marginLeft: pxToVw(18) } }} placeholder={t('New number')} value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+            </div>
+            <div className='flex flex-row' style={{ marginTop: pxToVw(20) }}>
+                {/* add country code */}
+                <Select style={{ width: pxToVw(70), height: pxToVw(35) }} options={sampleCodes} placeholder={"+971"} />
+                <Input styles={{ input: { width: pxToVw(422), height: pxToVw(35), fontSize: pxToVw(10), marginLeft: pxToVw(18) } }} placeholder={t('Confirm number')} value={confirmNumber} onChange={(e) => setConfirmNumber(e.target.value)} />
             </div>
       </Modal>
     )

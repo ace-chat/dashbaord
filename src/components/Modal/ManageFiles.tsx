f9e504bbf7d7c08001cff60a741f8a2678c9a86c
EsraaSaad298 <esraa_mohyeldin@hotmail.com>
2023-12-14 02:11:58 +0400

    Modal UI continue ..

--- src/components/Modal/ManageFiles.tsx
+++ src/components/Modal/ManageFiles.tsx
@@ -53,10 +53,49 @@ export const ManageFiles = ({t, manageFiles, toggleManageFiles, fileInput, handl
 
                 <div className="w-px bg-[#F3F3F3]"></div>
 
-                <div className={`flex p-24 flex-col`} style={{ width: pxToVw(500), justifyContent: "center" }}>
+                <div className={`flex p-24 flex-col`} style={{ width: pxToVw(500) }}>
+
                     <div style={{ fontFamily: "PingFang SC Medium", fontSize: pxToVw(22) }}>{t("Existing Files")}</div>
+                    <div className={`mt-12`}>
+                        <div className='flex flex-row rounded-4 mt-16' 
+                        style={{"backgroundColor": "white", "display": "flex", "width": pxToVw(400), "height": pxToVw(293), border: "1px solid #DBDBDB", flexDirection: "column" }}
+                        >
+                          {selectedFiles.map((file: any, index: number) => {
+                            return(
+                                <div className='flex flex-col items-center' key={index} style={{ marginTop: pxToVw(10), alignSelf: "self-start", paddingLeft: pxToVw(10)}}>
+                                    <Icon name='pdf' style={{ width: pxToVw(45), height: pxToVw(45), alignSelf: "center" }} />
+                                    <div style={{ fontFamily: "PingFang SC Medium", fontSize: pxToVw(6), color: "#818181", marginTop: pxToVw(5) }}>{file?.name}</div>
+                                </div>
+                            )
+                          })}
+                        </div> 
+                    </div>
+
+                    <div className='flex self-center' style={{ marginTop: pxToVw(30), marginBottom: pxToVw(30), height: pxToVw(39)}} />
                 </div>
             </div>
       </Modal>
     )
 };
+
+
+{/* <div className={`flex p-24 flex-col`} style={{ width: pxToVw(500), justifyContent: "center" }}>
+<div style={{ fontFamily: "PingFang SC Medium", fontSize: pxToVw(22) }}>{t("Existing Files")}</div>
+<div className={`mt-12`}>
+    <div className='flex rounded-8 mt-16 justify-center items-center' 
+    style={{"backgroundColor": "#F4F6FA", "display": "flex", "width": pxToVw(400), "height": pxToVw(356), border: "1px dashed #8B8B8B", flexDirection: "column" }}
+    onClick={() => fileInput.current.click()}>
+        <input
+            type="file"
+            id="fileInput"
+            accept=".pdf"
+            ref={fileInput}
+            onChange={handleFileInputChange}
+            style={{ display: "none" }}
+        />
+        <Icon name={'upload'} style={{ 'width': pxToVw(22), 'height': pxToVw(22), 'marginTop': pxToVw(5) }} />
+        <div className={`mt-2`} style={{ color: "#000", opacity: 0.6, fontSize: pxToVw(10), fontFamily: "PingFang SC Light" }}>{ t("Upload more files to knowledge base") }</div>
+        <div className={`mt-2`} style={{ color: "#000", opacity: 0.6, fontSize: pxToVw(10), fontFamily: "PingFang SC Light" }}>{ t("Drop files here") }</div>
+    </div> 
+</div>
+</div> */}

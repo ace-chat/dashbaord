f54376a9213ac59626d5d82267aae1da686338c1
alex <ch.10234en@gmail.com>
2024-01-22 12:54:44 +0400

    feat: chat with ace

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,15 @@
+import { message } from 'antd'
+
 export * from './format.ts'
 
 export const pxToVw = (n: number) => {
   return `${Number((n / 1728) * 100).toFixed(2)}vw`
+}
+
+export const copied = (text: string) => {
+  navigator.clipboard.writeText(text).then(() => {
+    message.success("Copied successfully").then()
+  }).catch(() => {
+    message.error("Copied failed").then()
+  })
 }

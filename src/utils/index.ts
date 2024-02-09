e133d86cd5651bc390664c496e8defd27551d8f8
Psi <>
2024-02-09 18:30:50 +0400

    fix: remove the new pxToVw function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,12 +4,16 @@ import copy from 'copy-to-clipboard'
 export * from './format.ts'
 
 export const pxToVw = (n: number) => {
-  const baseWidth = 1440; 
-  const vwUnit = (n / baseWidth) * 100;
-  const minFontSizePx = 12; 
-  const minFontSizeVw = (minFontSizePx / baseWidth) * 100;
-  return `${Math.max(vwUnit, minFontSizeVw).toFixed(2)}vw`;
-};
+  return `${Number((n / 1728) * 100).toFixed(2)}vw`
+}
+
+// export const pxToVw = (n: number) => {
+//   const baseWidth = 1440; 
+//   const vwUnit = (n / baseWidth) * 100;
+//   const minFontSizePx = 12; 
+//   const minFontSizeVw = (minFontSizePx / baseWidth) * 100;
+//   return `${Math.max(vwUnit, minFontSizeVw).toFixed(2)}vw`;
+// };
 
 export const copied = (text: string) => {
   copy(text)

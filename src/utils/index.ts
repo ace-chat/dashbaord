a5d9b6bfec8f014f5afb110a66cfd977302a899f
Psi <>
2024-02-09 18:14:24 +0400

    fix: ui fixes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,8 +4,12 @@ import copy from 'copy-to-clipboard'
 export * from './format.ts'
 
 export const pxToVw = (n: number) => {
-  return `${Number((n / 1728) * 100).toFixed(2)}vw`
-}
+  const baseWidth = 1440; 
+  const vwUnit = (n / baseWidth) * 100;
+  const minFontSizePx = 12; 
+  const minFontSizeVw = (minFontSizePx / baseWidth) * 100;
+  return `${Math.max(vwUnit, minFontSizeVw).toFixed(2)}vw`;
+};
 
 export const copied = (text: string) => {
   copy(text)

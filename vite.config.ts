99ecdcfa43b76475e078c2154f93bec30faaaf70
unknown <>
2024-02-20 12:13:14 +0400

    fix: save button not disabled

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://178.128.96.65",
+        // target: "http://127.0.0.1:8080",
         changeOrigin: true,
       }
     }

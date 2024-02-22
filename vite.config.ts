3d0d99ed331b4ee5cca3e24dc182b58304fe2783
alex <ch.10234en@gmail.com>
2024-02-22 14:50:16 +0800

    feat: change build config file

--- vite.config.ts
+++ vite.config.ts
@@ -48,13 +48,14 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id){
-          if (id.includes('tailwindcss')){
-            return 'tailwindcss'
-          }else{
-            return 'vendor'
+        manualChunks: (id) => {
+          if(id.includes('node_modules')){
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
-        }
+        },
+        entryFileNames: 'js/[name].[hash].js',
+        chunkFileNames: 'js/[name].[hash].js',
+        assetFileNames: '[ext]/[name].[hash].[ext]'
       }
     }
   },

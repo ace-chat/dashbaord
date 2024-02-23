250343f2af7015f931b25ed841fc80631e79bbe1
unknown <>
2024-02-23 17:04:18 +0400

    feat: language support

---
+++ src/constants/loginText.ts
@@ -0,0 +1,25 @@
+import Robo from '@/assets/robo.svg';
+import Tool from '@/assets/tool.svg';
+import Analytics from '@/assets/analytics.svg';
+
+export const LoginText = [
+    {
+        title: 'Smart Chatbot For Your Business',
+        heading: 'Business Chatbot',
+        content: 'You can create a knowledge base for the chatbot of your business, along with some questions and answers constructed by you, and we will create a generative-AI chatbot and deploy it on WhatsApp so you can convert leads for your business. ',
+        icon: Robo,
+    },
+    {
+        title: 'Best Content Generation Tools',
+        heading: 'Content  Generation',
+        content: 'Say goodbye to content creation hassles. Our AI-powered tool generates compelling content for social media ads and more, so you can focus on strategy. Empower your marketing with our AI content generation tool. From ads to blogs, turn ideas into impactful content. ',
+        icon: Tool,
+    },
+    {
+        title: 'Analytics Now Like Never Before.',
+        heading: 'Simple Analytics',
+        content: 'Uncover actionable insights effortlessly with our AI-driven Simple Analytics. From data to decisions, streamline your strategy.',
+        icon: Analytics,
+    }
+
+]
--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,20 +1,22 @@
 import LoginBox from '@/components/LoginBox/LoginBox'
 import Bg from '@/assets/bg.png'
 import LoginInfo from '@/components/LoginInfo/LoginInfo'
-import Robo from '@/assets/robo.svg'
+import { LoginText } from '@/constants/loginText'
+import { useTranslation } from 'react-i18next'
 
 const Login = () => {
+    const random = Math.floor(Math.random() * LoginText.length)
+    const { t } = useTranslation()
   return (
     <div
       className="flex flex-row p-90"
       style={{ height: '100vh', width: '100vw', background: `url(${Bg})` }}
     >
-      {/* send random title, heading, content, icon as props*/}
       <LoginInfo
-        title="title"
-        heading="heading"
-        content="content"
-        icon={Robo}
+        title={t(LoginText[random].title)}
+        heading={t(LoginText[random].heading)}
+        content={t(LoginText[random].content)}
+        icon={t(LoginText[random].icon)}
       />
       <LoginBox />
     </div>
